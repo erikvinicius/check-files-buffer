d8440a78d2d7f4f28a2a2cc2fd8f8d7feaef66f4
erikvinicius <erik.vinicius9911@gmail.com>
2020-12-19 15:45:03 -0300

    last release

---
+++ src/check-files-buffer/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckFilesBuffer.Models
+{
+    public class Item
+    {
+        public string Path { get; set; }
+        public byte[] Buffer { get; set; }
+    }
+}
--- src/check-files-buffer/Program.cs
+++ src/check-files-buffer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CheckFilesBuffer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace CheckFilesBuffer
@@ -7,11 +10,13 @@ namespace CheckFilesBuffer
     {
         static void Main(string[] args)
         {
-            Console.Title = "Check files buffer v1.0";
-            Console.WriteLine("Check files buffer v1.0");
+
+            Console.Title = "Check files buffer integrity";
+            Console.WriteLine("Check files buffer v1.1");
             Console.WriteLine("Developer: Erik Vinicius");
             Console.WriteLine("Max supported files: 2\n");
-            if(args.Length <= 1)
+
+            if (args.Length <= 1)
             {
                 Console.WriteLine("Error. More than one file is needed to check the buffer.");
                 Console.WriteLine("Press any key to exit.");
@@ -19,50 +24,79 @@ namespace CheckFilesBuffer
                 Environment.Exit(Environment.ExitCode);
                 return;
             }
-            else if(args.Length > 2)
+            else if (args.Length > 2)
             {
                 Console.WriteLine("Error. Max supported files: 2");
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadKey();
                 Environment.Exit(Environment.ExitCode);
             }
-            Console.WriteLine("Loading {0} files...\n", args.Length);
-            for(int i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine("Loaded File. Path:{0} Length:{1}", args[i], File.ReadAllBytes(args[i]).Length);
-            }
-            Console.WriteLine("\nChecking files length...\n");
-            var bufferOne = File.ReadAllBytes(args[0]);
-            var bufferTwo = File.ReadAllBytes(args[1]);
-            if(bufferOne.Length != bufferTwo.Length)
-            {
-                Console.WriteLine("\nFiles length not equals.");
-                Console.WriteLine("Press any key to exit.");
-                Console.ReadKey();
-                Environment.Exit(Environment.ExitCode);
-                return;
-            }
-            Console.WriteLine("Files length equals. [{0}/{1}]\n", bufferOne.Length, bufferTwo.Length);
-            int offset = 0;
-            int bufferInvalid = 0;
-            Console.WriteLine("Checking files buffer...\n");
-            for (int i = 0; i < bufferOne.Length; i++)
-            {
-                if (bufferOne[i] != bufferTwo[i])
-                {
-					bufferInvalid++;
-                    int invalidOffset = offset;
-                    Console.WriteLine("Invalid Offset:" + invalidOffset);
-                }
-                offset++;
-            }
-            if (bufferInvalid > 0)
-                Console.WriteLine("Invalid buffers count:{0}", bufferInvalid);
-            else
-                Console.WriteLine("Success files buffer equals.");
 
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            List<Item> files = new List<Item>();
+            Console.WriteLine("Loading {0} files...\n", args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                var filePath = args[i];
+                var fileBuffer = File.ReadAllBytes(filePath);
+
+                Item item = new Item
+                {
+                    Path = filePath,
+                    Buffer = fileBuffer
+                };
+
+                files.Add(item);
+
+                Console.WriteLine("Loaded File:\n\tPath:{0}\n\tLength:{1}\n", filePath, fileBuffer.Length);
+            }
+            Console.WriteLine("Loaded {0} files...\n", files.Count);
+
+            Console.WriteLine("Checking files length...\n");
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                var item = files[i];
+                var nextItem = files[i + 1];
+
+                if ((files.Count - 1) < (i + 1))
+                    return;
+
+                if (item.Buffer.Length != nextItem.Buffer.Length)
+                {
+                    Console.WriteLine("Files length not equals:");
+                    Console.WriteLine("\tPath:{0}\n\tLength:{1}\n", item.Path, item.Buffer.Length);
+                    Console.WriteLine("\tPath:{0}\n\tLength:{1}", nextItem.Path, nextItem.Buffer.Length);
+                    Console.WriteLine("\nPress any key to exit.");
+                    Console.ReadKey();
+                    Environment.Exit(Environment.ExitCode);
+                    return;
+                }
+                Console.WriteLine("Files length equals:", item.Buffer.Length, nextItem.Buffer.Length);
+                Console.WriteLine("\tPath:{0}\n\tLength:{1}\n", item.Path, item.Buffer.Length);
+                Console.WriteLine("\tPath:{0}\n\tLength:{1}\n", nextItem.Path, nextItem.Buffer.Length);
+
+
+                Console.WriteLine("Checking files buffer integrity...\n");
+                int offset = 0;
+                int bufferInvalid = 0;
+                for (int j = 0; j < item.Buffer.Length; j++)
+                {
+                    if (item.Buffer[j] != nextItem.Buffer[j])
+                    {
+                        bufferInvalid++;
+                        int invalidOffset = offset;
+                        Console.WriteLine("Invalid offset:{0}", invalidOffset);
+                    }
+                    offset++;
+                }
+                if (bufferInvalid > 0)
+                    Console.WriteLine("\nInvalid buffers count:{0}", bufferInvalid);
+                else
+                    Console.WriteLine("Success files buffer equals.");
+
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+            }
         }
     }
 }
